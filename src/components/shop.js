d835931c29702d13de708c89b6c3208fc3c49741
minfromdavis <minchin903@gmail.com>
2024-01-30 16:43:55 -0800

    washingtown app

---
+++ src/components/shop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+
+function Shop() {
+  return (
+    <div>
+       <h3>The Shop</h3>
+    </div>
+  );
+}
+
+export default Shop
---
+++ src/components/stocks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Stocks =()=> {
+    return (    
+                <div className='navBar'>
+                    <h3>Hello from stocks</h3>
+                </div>
+    )
+
+}
+
+export default Stocks;
